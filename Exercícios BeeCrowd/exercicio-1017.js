64cc90ce0de31d1043e385ae9639e61913533426
Lorenzzo Antonio Patatt <lorenzzoapatatt2209@gmail.com>
2025-04-07 19:19:13 -0300

    outros desafios do BeeCrowd

--- Exercícios BeeCrowd/exercicio-1017.js
+++ Exercícios BeeCrowd/exercicio-1017.js
@@ -2,8 +2,9 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
-var tempo = parseInt(lines[0]/60);
-var velocidade = parseInt(lines[1]/1000);
-var calculo = 
+automovel = 12
+var tempoGasto = parseInt(lines[0]);
+var tempoMedio = parseInt(lines[1]);
+var quantidadeParaViagem = (tempoGasto * tempoMedio)/automovel;
 
-console.log(tempo);
+console.log(quantidadeParaViagem.toFixed(3)); 
