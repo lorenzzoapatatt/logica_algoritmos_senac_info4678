4009e41569b19052b72a29f1ac639b6fd759637f
Lorenzzo Antonio Patatt <lorenzzoapatatt2209@gmail.com>
2025-04-14 20:16:39 -0300

    fazendo mais exercicio

--- Exercícios BeeCrowd/exercicio-1018.js
+++ Exercícios BeeCrowd/exercicio-1018.js
@@ -1,7 +1,20 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
-var A = parseInt(lines[0]) //segundos
-var calculo = A /60;
+var valor = parseInt(lines[0]);
+var valorCem = Math.floor(valor / 100);
+var restoDeCem = valor % 100;
+var restoDeCinquenta = restoDeCem % 50;
+var restoDeVinte = restoDeCinquenta % 20;
+var restoDeDez = restoDeVinte % 10;
+var restoDeCinco = restoDeDez % 5;
+var restoDeUm = restoDeCinco % 1 ;
 
-console.log()
+
+console.log(`${valor}`);
+console.log(`${valorCem} nota(s) de R$ 100,00`);
+console.log(`${restoDeCinquenta} nota(s) de R$ 50,00`);
+console.log(`${restoDeVinte} nota(s) de R$ 20,00`);
+console.log(`${restoDeDez} nota(s) de R$ 10,00`);
+console.log(`${restoDeCinco} nota(s) de R$ 5,00`);
+console.log(`${restoDeUm} nota(s) de R$ 1,00`);
--- Exercícios BeeCrowd/exercicio-1020.js
+++ Exercícios BeeCrowd/exercicio-1020.js
@@ -3,9 +3,10 @@ var lines = input.split('\n');
 
 var diasPessoa = parseInt(lines[0]);
 
-var anosPessoa = parseInt(diasPessoa/360);
-var anosMeses = parseInt(diasPessoa/30);
-var anosDias = diasPessoa;
-
+var anosPessoa = Math.trunc(diasPessoa/365);
+var anosResto = diasPessoa%365;
+var mesesPessoa = Math.trunc(anosResto/30);
+var mesesResto = mesesPessoa%30;
+var anosDias = Math.trunc(mesesResto);
 
 console.log(`${anosPessoa} ano(s) \n ${anosMeses} mes(es) \n ${anosDias} dia(s)`);
